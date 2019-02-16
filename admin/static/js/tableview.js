1aedcca48243ce2be658b7393845358c77f171d0
Sam Hughes <sam@samuelhughes.com>
2019-02-15 19:44:53 -0800

    tableview.js extant

---
+++ admin/static/js/tableview.js
@@ -0,0 +1,15 @@
+// (function() {
+    function foo() {
+        console.log("This is foo.");
+    }
+
+    function bar() {
+        console.log("Bar");
+    }
+
+    function baz() { 
+        console.log("Baz");
+    }
+
+
+// })();
